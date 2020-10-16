4eb72ce68e1245833efdd5592cf0e9596d6f3c96
V. Lai <lai.vivian@gmail.com>
2020-10-15 20:33:27 -0700

    ui-tasks-date-picker: routes & datepicker progress

---
+++ src/utils/constants.js
@@ -0,0 +1,16 @@
+export const MONTHS_IN_YEAR = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+export const DAYS_IN_MONTHS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]; // don't forget about leap years
