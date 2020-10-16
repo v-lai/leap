568756476e2f17e74a4b8e5fc5054b1b07f3f4d6
V. Lai <lai.vivian@gmail.com>
2020-10-15 21:21:58 -0700

    ui-tasks-date-picker: change colors & update skill color picker

--- src/components/main/CreateTask/SkillColorOptions.js
+++ src/components/main/CreateTask/SkillColorOptions.js
@@ -1,23 +1,43 @@
 import React from 'react';
 import { Button } from '../../base/Button/Button';
+import {
+  bluegrey,
+  darkNavy,
+  forest,
+  grapefruit,
+  greyLime,
+  greyPurple,
+  lavender,
+  orange,
+  peacock,
+  perrywinkle,
+  powderBlue,
+  softPink,
+  steelGrey,
+  teal,
+  wheat,
+  wisteria,
+  black25,
+  white,
+} from '../../../themes/theme';
 
 const COLOR_OPTIONS = [
-  '#FF8074',
-  '#003A4F',
-  '#34563E',
-  '#6F6888',
-  '#FFA865',
-  '#657677',
-  '#3D9D7A',
-  '#7F91C6',
-  '#FF9CC4',
-  '#6AA4BC',
-  '#AED79B',
-  '#B180BB',
-  '#FCDD7D',
-  '#B2D6EA',
-  '#AEDFD6',
-  '#C8AAD0',
+  grapefruit,
+  darkNavy,
+  forest,
+  greyPurple,
+  orange,
+  steelGrey,
+  peacock,
+  perrywinkle,
+  softPink,
+  bluegrey,
+  greyLime,
+  wisteria,
+  wheat,
+  powderBlue,
+  teal,
+  lavender,
 ];
 
 export default function SkillColorOptions({
@@ -29,13 +49,29 @@ export default function SkillColorOptions({
     <div
       style={{
         display: 'grid',
+        width: '15.625rem',
+        height: '13.75rem',
+        borderRadius: '10px',
         gridTemplateColumns: '1fr 1fr 1fr 1fr',
-        gridTemplateRows: '50px 50px 50px 50px 30px',
+        gridTemplateRows: '2rem 1fr 1fr 1fr 1fr 0.25fr',
         justifyItems: 'center',
         alignItems: 'center',
-        backgroundColor: '#FFF',
+        backgroundColor: white,
       }}
     >
+      <p
+        style={{
+          gridColumnStart: 1,
+          gridColumnEnd: 5,
+          height: '0.938rem',
+          lineHeight: '1rem',
+          alignSelf: 'center',
+          fontSize: '0.813rem',
+          fontWeight: 'bold',
+        }}
+      >
+        Click to select a color
+      </p>
       {COLOR_OPTIONS.map((color) => (
         <button
           key={color}
@@ -43,25 +79,21 @@ export default function SkillColorOptions({
           aria-label={`color-${color}`}
           style={{
             backgroundColor: color,
-            height: '1.5rem',
-            width: '1.5rem',
+            height: '1.563rem',
+            width: '1.563rem',
             MozBorderRadius: '50%',
             WebkitBorderRadius: '50%',
             borderRadius: '50%',
             KhtmlBorderRadius: '50%',
-            border: color === skillColor ? '2px solid black' : 'none',
+            border: 'none',
+            boxShadow: color === skillColor ? 'none' : `0 4px 4px 0 ${black25}`,
           }}
-          onClick={() => setColorSelection(color)}
+          onClick={() => {
+            setColorSelection(color);
+            done(false);
+          }}
         />
       ))}
-      <Button
-        type="button"
-        className="button"
-        style={{ gridColumnStart: 4, lineHeight: '1rem' }}
-        onClick={() => done(false)}
-      >
-        Done
-      </Button>
     </div>
   );
 }
