4e541395c65b15a6633ffa492dfd932ce2679df0
V. Lai <lai.vivian@gmail.com>
2020-10-18 18:51:44 -0700

    ui-tasks-date-picker: update CreateTask & select color modal

--- src/components/main/CreateTask/styles.js
+++ src/components/main/CreateTask/styles.js
@@ -5,27 +5,20 @@ const Container = styled.div`
   width: 100vw;
   height: 100vh;
   display: grid;
-  padding: 1.5rem max(1rem, calc(100vw - 400px) / 2) 2rem
-    max(1rem, calc(100vw - 400px) / 2);
   align-items: center;
   justify-items: center;
   overflow-y: scroll;
 
-  & > div {
-    width: min(80vw, 400px);
+  label.taskName {
+    font-size: 0.813rem;
+    font-weight: bold;
+    line-height: normal;
   }
 
-  .rmc-date-picker {
-    & .rmc-picker:nth-child(1) {
-      order: 2;
-    }
-    & .rmc-picker:nth-child(2) {
-      order: 1;
-      flex: 2 2 0% !important;
-    }
-    & .rmc-picker:nth-child(3) {
-      order: 0;
-    }
+
+  #start-date,
+  #end-date {
+    width: auto;
   }
 `;
 
